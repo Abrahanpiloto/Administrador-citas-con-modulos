074120c319262ad743100a68bd4a7563976d535a
Abrahan <abrahanpiloto@gmail.com>
2022-10-18 16:20:48 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+import {nuevaCita, datosCita} from "./funciones.js";
+import {mascotaInput,
+    propietarioInput,
+    telefonoInput,
+    fechaInput,
+    horaInput,
+    sintomasInput,
+    formulario
+} from "./selectores.js";
+
+formulario.addEventListener('submit', nuevaCita);
+
+// Eventos
+eventListeners();
+function eventListeners() {
+    mascotaInput.addEventListener('change', datosCita);
+    propietarioInput.addEventListener('change', datosCita);
+    telefonoInput.addEventListener('change', datosCita);
+    fechaInput.addEventListener('change', datosCita);
+    horaInput.addEventListener('change', datosCita);
+    sintomasInput.addEventListener('change', datosCita);
+}
+
+
+
+
+
+
+
+
